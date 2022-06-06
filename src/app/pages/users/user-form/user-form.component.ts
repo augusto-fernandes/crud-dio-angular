5c6b8150c9289cf292839ddc0391c9653ad01a12
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-06-06 19:23:13 -0300

    Desenvolvendo a classe UserFormComponent

--- src/app/pages/users/user-form/user-form.component.ts
+++ src/app/pages/users/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+  userForm: FormGroup;
 
-  constructor() { }
-
+  constructor(private fb:FormBuilder) { 
+  this.userForm = this.fb.group({
+    id: 0,
+    nome: '',
+    sobrenome: '',
+    idade: '',
+    profissao:'',
+  })
+}
   ngOnInit(): void {
   }
 
+  createUser(){
+    console.log(this.userForm.value)
+  }
 }
