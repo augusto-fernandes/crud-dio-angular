0798839ea6f74aa93dc56de1fe1050da1625873e
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-06-07 18:32:08 -0300

    Criando o componente todo-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { UsersListComponent } from './pages/users/users-list/users-list.component';
 import { UserFormComponent } from './pages/users/user-form/user-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TodoListComponent } from './pages/todo/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HeaderComponent,
     UsersListComponent,
     UserFormComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
